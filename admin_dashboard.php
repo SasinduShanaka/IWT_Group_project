39a6eaa7e445c9b133f15db6744c75b0af009350
praneesha1516git <praneesha2003perera@gmail.com>
2024-09-20 18:28:39 +0530

    praneesha
    home , admin dashboard

---
+++ admin_dashboard.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="./css/admin_dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Protest+Guerrilla&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+    <section class="products">
+        <h2 class="headings">Welcome Admin!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem vitae accusantium sint explicabo quia aspernatur, sunt in perferendis! Saepe atque ea cumque laborum, nemo vero in expedita omnis doloremque necessitatibus tenetur quasi. Perspiciatis iure minima qui ea esse quod tenetur non libero suscipit recusandae, quibusdam repellendus hic sed consequatur eius, animi vitae dignissimos laudantium temporibus numquam, nam autem totam? Asperiores vel deserunt exercitationem assumenda quidem repudiandae? Sint voluptatibus nulla nesciunt, odit sunt officiis ullam illum.</p>
+        <div class="card-container">
+            <div class="card" id="card1"  >
+                <div class="card-content" >
+                <img src="./images/fam.png" alt="Card Image" class="card-image">
+                <h3>Life Insurance <br>Plans </h3>
+                <a href="www.google.com" class="learn-more">Learn More</a>
+                </div>
+            </div>
+            <div class="card" id="card2">
+                <div class="card-content">
+                <img src="./images/retire.png" alt="Card Image" class="card-image">
+                <h3>Retirement Plans</h3>
+                <a href="#" class="learn-more">Learn More</a>
+                </div>
+            </div>
+            <div class="card" id="card3">
+                <div class="card-content">
+                <img src="./images/health.png" alt="Card Image" class="card-image">
+                <h3>Medical <br>Plans</h3>
+                <a href="#" class="learn-more">Learn More</a>
+                </div>
+            </div>
+
+            <div class="card" id="card4">
+                <div class="card-content">
+                <img src="./images/inv.png" alt="Card Image" class="card-image">
+                <h3>Investment Plans</h3>
+               
+                <a href="#" class="learn-more">Learn More</a>
+                </div>
+            </div>
+
+            <div class="card" id="card5">
+                <div class="card-content">
+                <img src="./images/group.png" alt="Card Image" class="card-image">
+                <h3>Group <br>Plans</h3>
+                <a href="#" class="learn-more">Learn More</a>
+                </div>
+            </div>
+
+            <div class="card" id="card6">
+                <div class="card-content">
+                <img src="./images/group.png" alt="Card Image" class="card-image">
+                <h3>Group <br>Plans</h3>
+                <a href="#" class="learn-more">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+</body>
+
+
+
+</html>
