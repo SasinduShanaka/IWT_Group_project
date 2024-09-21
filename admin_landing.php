a48b759adb9b63bd2e367cd890d0916be0fd025e
praneesha1516git <praneesha2003perera@gmail.com>
2024-09-21 18:18:31 +0530

    aded admin landing page

---
+++ admin_landing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fade In Text</title>
+    <link rel="stylesheet" href="./style/admin_landing.css">
+</head>
+<body>
+
+    <!-- The text that will fade in -->
+    <div class="fade-in-text" id="fadeText">
+    <h1>Welcome to Admin Dashboard</h1>
+    <p>Hello Praneesha !</p>
+    <button class="button" style="justify-content: center;"><span>Enter</span></button>
+</div>
+
+    <script src="./js/admin_landing.js"></script>
+</body>
+</html>
+
